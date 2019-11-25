4bb60348a1a6811d5eac9e5f233fde637f61ad97
Klayton leandro <klayton_leandro54@hotmail.com>
2019-11-25 11:50:20 -0300

    users material-ui-core

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,8 +4,8 @@ import Table from 'react-bootstrap/Table'
 import api from '~/services/api';
 import { Container } from './styles';
 import { Link } from 'react-router-dom';
-
-
+import PropTypes from 'prop-types';
+import TablePagination from '@material-ui/core/TablePagination';
 
 export default function Dashboard() {
 
@@ -26,8 +26,60 @@ const [cpf, setCPF] = useState('');
   useEffect(() => {
     loadUsers();
   },[]);
+  
+  function TablePaginationActions(props) {
+    const { count, page, rowsPerPage, onChangePage } = props;
+  
+    const handleFirstPageButtonClick = event => {
+      onChangePage(event, 0);
+    };
+  
+    const handleBackButtonClick = event => {
+      onChangePage(event, page - 1);
+    };
+  
+    const handleNextButtonClick = event => {
+      onChangePage(event, page + 1);
+    };
+  
+    const handleLastPageButtonClick = event => {
+      onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
 
+    <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    TablePaginationActions.propTypes = {
+      count: PropTypes.number.isRequired,
+      onChangePage: PropTypes.func.isRequired,
+      page: PropTypes.number.isRequired,
+      rowsPerPage: PropTypes.number.isRequired,
+    };
+    
   return (
+
+    
     <Container>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <input className="form-control mr-sm-2" type="search" placeholder="" aria-label="Search" value={cpf} onChange={(e) => setCPF(e.target.value)}></input>
@@ -84,6 +136,23 @@ const [cpf, setCPF] = useState('');
           }
         </tbody>
       </Table>
+      <TableRow>
+      <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { 'aria-label': 'rows per page' },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
     </Container>
   );
 }
