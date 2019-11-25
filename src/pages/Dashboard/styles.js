04268236a93d42291cc1c52b726536e9c67a4062
Klayton leandro <klayton_leandro54@hotmail.com>
2019-11-25 13:22:05 -0300

    styels in folder document in auth is default

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 500px;
-  margin: 30px auto;
+  margin: 60px 160px;
 
   display: auto;
-  flex-direction: column;
+ 
 `;
