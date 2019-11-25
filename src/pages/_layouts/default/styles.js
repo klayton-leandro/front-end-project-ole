c3e782a8870013c7937d2b82241ae354611ffe51
Klayton leandro <klayton_leandro54@hotmail.com>
2019-11-25 12:24:37 -0300

    styels in folder document in auth is default

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #ff0000, #ff0066);
-  display: flex;
+  display: block;
  
 `;
